0a1246128c9977a400231ccda3cd3744fb43895e
iamkapilll <kapildevchaudhary067@gmail.com>
2025-09-21 23:31:04 +0545

    herosection injected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
-    <h1 className="text-center text-2xl">hello world</h1>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <HeroSection />
+
+    </main>
+
   );
 }
